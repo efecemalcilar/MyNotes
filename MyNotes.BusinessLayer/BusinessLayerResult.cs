488c8a1fb1c7948a9991056aeaf0f6867c9075b1
Efe İlhan Cemalcılar <efecemalcilar@gmail.com>
2022-03-29 14:49:20 +0300

    Manager classes, Error Messages , Partial Category,footer,navigation

---
+++ MyNotes.BusinessLayer/BusinessLayerResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyNotes.EntityLayer;
+using MyNotes.EntityLayer.Messages;
+
+namespace MyNotes.BusinessLayer
+{
+    public class BusinessLayerResult<T> where T: class //Bunun bütün entity varlıklarda geçerli olması için T tipinde generic hale getiriyorum.
+    {
+        public List<ErrorMessageObj> Errors { get; set; } 
+
+        public T Result { get; set; }
+
+        public BusinessLayerResult()
+        {
+            Errors = new List<ErrorMessageObj>();
+        }
+
+        public void AddError(ErrorMessageCode code, string message) //Business tarafında add error metodunu kullanıcam
+        {
+            Errors.Add(new ErrorMessageObj{Code = code , Message = message});
+
+        }
+    }
+}
